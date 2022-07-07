6952f1bc92f9d892faf5cb066b21b454c8a364c7
Fabricio Silva <F.sillva2507@gmail.com>
2022-07-07 20:59:57 -0300

    usando o map

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // Quando não declaro o tipo de metodo usando fecth,
 // ele entende que é o GET (traz informações)
-const response = fetch('https://jsonplaceholder.typicode.com/posts')
+const response = fetch('https://jsonplaceholder.typicode.com/users')
 //Promisse que trata o sucesso é o método then(), 
 // o parâmetro res recebe o retorno da função anterior
 .then(res =>{
-console.log(res.json())
+console.log(res)
+return res.json();
 })
+.then(data => {
+    for (var i = 0; i < data.length; i++){
+        console.log(`\nPessoa ${i+1}`)
+        console.log(data[i].name)
+        console.log(data[i].email)
+
+    }
+    
+})
+// Usamos para inteirar (percorrer o array), o map()
+// que é um método para percorrer todo o array como um for
+// .then(data => {
+//     data.map(user => {
+//         console.log(`Nome: ${user.name}`,`Email: ${user.email}`)
+//     })
+// })
 // Promisse que trata o erro é o método catch()
 .catch(error => {
     console.log(`Deu erro na Busca:${error}`)
